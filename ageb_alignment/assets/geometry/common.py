4f26dfda25cfb5a1c8dbd33244b9807628e02707
Gonzalo Peraza <ggperaza@gmail.com>
2024-10-11 10:37:52 -0600

    added replacement lists for 1990->2000

--- ageb_alignment/assets/geometry/common.py
+++ ageb_alignment/assets/geometry/common.py
@@ -28,3 +28,30 @@ def fix_overlapped(gdf: gpd.GeoDataFrame, cover_list: list) -> gpd.GeoDataFrame:
         ]
 
     return gdf
+
+
+def replace_geoms(
+    gdf_old: gpd.GeoDataFrame,
+    gdf_new: gpd.GeoDataFrame,
+    replace_list: list,
+    check_complete=False,
+) -> gpd.GeoDataFrame:
+    """Replaces geometries in old with the union of geometries in new following the
+    corresponding relations in replace_list."""
+
+    gdf_old = gdf_old.copy()
+
+    old_idx = [oi for oi, _ in replace_list]
+    targets = [tl for _, tl in replace_list]
+
+    if check_complete:
+        assert np.all(sorted(old_idx) == gdf_old.index)
+
+    # Check targets are unique
+    targets_flat = sum(targets, [])
+    assert len(np.unique(targets_flat)) == len(targets_flat)
+
+    for old_id, new_ids in replace_list:
+        gdf_old.loc[old_id, "geometry"] = gdf_new.loc[new_ids, "geometry"].union_all()
+
+    return gdf_old

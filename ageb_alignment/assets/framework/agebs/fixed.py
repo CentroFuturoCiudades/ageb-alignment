48345155ee572854d9ad8983ffdfeb1a9226512e
Rodolfo Figueroa <4rodolfofigueroa2@gmail.com>
2024-10-17 22:04:39 -0600

    Partial work to translate merging to ops

--- ageb_alignment/assets/framework/agebs/fixed.py
+++ ageb_alignment/assets/framework/agebs/fixed.py
@@ -1,7 +1,9 @@
 import geopandas as gpd
+import pandas as pd
 
+from ageb_alignment.assets.framework.agebs import initial as ageb_prep
 from ageb_alignment.resources import PathResource
-from dagster import asset, AssetExecutionContext, AssetIn
+from dagster import asset, graph_asset, AssetExecutionContext, AssetIn
 from pathlib import Path
 
 
@@ -9,14 +11,21 @@ def agebs_manual_factory(year: int) -> asset:
     @asset(
         name=str(year),
         key_prefix=["framework", "agebs"],
-        ins={"agebs": AssetIn(key=["agebs_initial", str(year)])},
         io_manager_key="gpkg_manager",
+        ins={
+            "ageb": AssetIn(key=["1990", "ageb"]),
+            "geometry": AssetIn(key=["geometry", "1990"]),
+        },
     )
     def _asset(
         context: AssetExecutionContext,
         path_resource: PathResource,
-        agebs: gpd.GeoDataFrame,
+        ageb: pd.DataFrame,
+        geometry: gpd.GeoDataFrame,
     ) -> gpd.GeoDataFrame:
+        prep_func = getattr(ageb_prep, f"prep_agebs_{year}")
+        agebs = prep_func(geometry, ageb)
+
         manual_path = Path(path_resource.intermediate_path) / f"replacement/{year}.gpkg"
         if manual_path.exists():
             df_replacement = gpd.read_file(manual_path).set_index("CVEGEO")

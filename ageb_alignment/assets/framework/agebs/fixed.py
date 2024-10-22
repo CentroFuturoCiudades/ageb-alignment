c70b9eebb0f03305bd8df5bec7ed9f6ad1456b60
RodolfoFigueroa <4rodolfofigueroa2@gmail.com>
2024-10-21 19:01:54 -0600

    Revert "Merge branch 'dev' into main"
    This reverts commit 46a1162722aca654e3d59420d178929f38e6bcaa, reversing
    changes made to 14c420942047a368826775efe3a1e862e986e543.

--- ageb_alignment/assets/framework/agebs/fixed.py
+++ ageb_alignment/assets/framework/agebs/fixed.py
@@ -1,9 +1,7 @@
 import geopandas as gpd
-import pandas as pd
 
-from ageb_alignment.assets.framework.agebs import initial as ageb_prep
 from ageb_alignment.resources import PathResource
-from dagster import asset, graph_asset, AssetExecutionContext, AssetIn
+from dagster import asset, AssetExecutionContext, AssetIn
 from pathlib import Path
 
 
@@ -11,21 +9,14 @@ def agebs_manual_factory(year: int) -> asset:
     @asset(
         name=str(year),
         key_prefix=["framework", "agebs"],
+        ins={"agebs": AssetIn(key=["agebs_initial", str(year)])},
         io_manager_key="gpkg_manager",
-        ins={
-            "ageb": AssetIn(key=["1990", "ageb"]),
-            "geometry": AssetIn(key=["geometry", "1990"]),
-        },
     )
     def _asset(
         context: AssetExecutionContext,
         path_resource: PathResource,
-        ageb: pd.DataFrame,
-        geometry: gpd.GeoDataFrame,
+        agebs: gpd.GeoDataFrame,
     ) -> gpd.GeoDataFrame:
-        prep_func = getattr(ageb_prep, f"prep_agebs_{year}")
-        agebs = prep_func(geometry, ageb)
-
         manual_path = Path(path_resource.intermediate_path) / f"replacement/{year}.gpkg"
         if manual_path.exists():
             df_replacement = gpd.read_file(manual_path).set_index("CVEGEO")
--- ageb_alignment/assets/framework/agebs/initial.py
+++ ageb_alignment/assets/framework/agebs/initial.py
@@ -1,14 +1,26 @@
 import geopandas as gpd
 import pandas as pd
 
+from ageb_alignment.resources import PathResource
 from ageb_alignment.types import GeometryTuple
-from dagster import op, AssetIn
+from dagster import asset, AssetIn
+from pathlib import Path
 
 
-@op
-def prep_agebs_1990(
-    geometry_1990: GeometryTuple, ageb_1990: pd.DataFrame
+@asset(
+    name="1990",
+    key_prefix="agebs_initial",
+    ins={
+        "ageb_1990": AssetIn(key=["1990", "ageb"]),
+        "geometry_1990": AssetIn(key=["geometry", "1990"]),
+    },
+)
+def agebs_1990_initial(
+    path_resource: PathResource, geometry_1990: GeometryTuple, ageb_1990: pd.DataFrame
 ) -> gpd.GeoDataFrame:
+    out_path = Path(path_resource.out_path) / "framework/agebs"
+    out_path.mkdir(exist_ok=True, parents=True)
+
     merged = (
         geometry_1990.ageb.join(ageb_1990, how="left")
         .fillna(0)
@@ -19,18 +31,38 @@ def prep_agebs_1990(
     return merged
 
 
-@op
-def prep_agebs_2000(
-    geometry_2000: GeometryTuple, ageb_2000: pd.DataFrame
+@asset(
+    name="2000",
+    key_prefix="agebs_initial",
+    ins={
+        "ageb_2000": AssetIn(key=["2000", "ageb"]),
+        "geometry_2000": AssetIn(key=["geometry", "2000"]),
+    },
+)
+def agebs_2000_initial(
+    path_resource: PathResource, geometry_2000: GeometryTuple, ageb_2000: pd.DataFrame
 ) -> gpd.GeoDataFrame:
+    out_path = Path(path_resource.out_path) / "framework/agebs"
+    out_path.mkdir(exist_ok=True, parents=True)
+
     merged = geometry_2000.ageb.join(ageb_2000, how="left").sort_index()
     return merged
 
 
-@op
-def prep_agebs_2010(
-    geometry_2010: GeometryTuple, ageb_2010: pd.DataFrame
+@asset(
+    name="2010",
+    key_prefix="agebs_initial",
+    ins={
+        "ageb_2010": AssetIn(key=["2010", "ageb"]),
+        "geometry_2010": AssetIn(key=["geometry", "2010"]),
+    },
+)
+def agebs_2010_initial(
+    path_resource: PathResource, geometry_2010: GeometryTuple, ageb_2010: pd.DataFrame
 ) -> gpd.GeoDataFrame:
+    out_path = Path(path_resource.out_path) / "framework/agebs"
+    out_path.mkdir(exist_ok=True, parents=True)
+
     merged = (
         geometry_2010.ageb.drop(
             columns=[
@@ -55,10 +87,20 @@ def prep_agebs_2010(
     return merged
 
 
-@op
-def prep_agebs_2020(
-    geometry_2020: GeometryTuple, ageb_2020: pd.DataFrame
+@asset(
+    name="2020",
+    key_prefix="agebs_initial",
+    ins={
+        "ageb_2020": AssetIn(key=["2020", "ageb"]),
+        "geometry_2020": AssetIn(key=["geometry", "2020"]),
+    },
+)
+def agebs_2020_initial(
+    path_resource: PathResource, geometry_2020: GeometryTuple, ageb_2020: pd.DataFrame
 ) -> gpd.GeoDataFrame:
+    out_path = Path(path_resource.out_path) / "framework/agebs"
+    out_path.mkdir(exist_ok=True, parents=True)
+
     merged = (
         geometry_2020.ageb.drop(columns="Ambito")
         .assign(

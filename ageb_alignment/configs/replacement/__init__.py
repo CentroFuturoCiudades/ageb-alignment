14886a09c951343f219564ceac8984fbc0d60faf
RodolfoFigueroa <4rodolfofigueroa2@gmail.com>
2024-10-21 19:15:11 -0600

    Fix imports

--- ageb_alignment/configs/replacement/__init__.py
+++ ageb_alignment/configs/replacement/__init__.py
@@ -1,2 +1,6 @@
-from ageb_alignment.configs.replacement import replace_1990_with_2000 as replace_1990
-from ageb_alignment.configs.replacement import replace_2000_with_2010 as replace_2000
+from ageb_alignment.configs.replacement.replace_1990_with_2000 import (
+    replace_1990_with_2000 as replace_1990,
+)
+from ageb_alignment.configs.replacement.replace_2000_with_2010 import (
+    replace_2000_with_2010 as replace_2000,
+)

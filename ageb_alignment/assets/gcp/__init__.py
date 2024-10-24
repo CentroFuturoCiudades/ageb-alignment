9dc785a504fbcaf43a494b0054e01d541e4b042c
RodolfoFigueroa <4rodolfofigueroa2@gmail.com>
2024-10-23 21:09:09 -0600

    Move final GCP loading to Ops

--- ageb_alignment/assets/gcp/__init__.py
+++ ageb_alignment/assets/gcp/__init__.py
@@ -1,10 +1,7 @@
-from ageb_alignment.assets.gcp import initial, final
+from ageb_alignment.assets.gcp import initial
 from dagster import Definitions, load_assets_from_modules
 
 
 defs = Definitions(
-    assets=(
-        load_assets_from_modules([initial], group_name="gcp_initial")
-        + load_assets_from_modules([final], group_name="gcp_final")
-    )
+    assets=(load_assets_from_modules([initial], group_name="gcp_initial"))
 )
--- ageb_alignment/assets/translate.py
+++ ageb_alignment/assets/translate.py
@@ -1,10 +1,13 @@
 import os
+import tempfile
 
+import geopandas as gpd
 import numpy as np
+import pandas as pd
 
 from ageb_alignment.partitions import zone_partitions
 from ageb_alignment.resources import PathResource
-from dagster import asset, AssetExecutionContext, AssetIn
+from dagster import graph_asset, op, AssetIn, In, OpDefinition, OpExecutionContext, Out
 from osgeo import gdal
 from pathlib import Path
 
@@ -12,6 +15,7 @@ from pathlib import Path
 gdal.UseExceptions()
 
 
+@op
 def generate_options_str(gcp: np.ndarray) -> str:
     options_str = "-tps -t_srs EPSG:6372 "
     for row in gcp:
@@ -19,62 +23,98 @@ def generate_options_str(gcp: np.ndarray) -> str:
     return options_str
 
 
-def prep_dir(path_resource: PathResource, context: AssetExecutionContext):
-    out_dir = Path(path_resource.out_path) / "/".join(context.asset_key.path)
-    out_dir.mkdir(exist_ok=True, parents=True)
-    return out_dir
+@op(
+    ins={
+        "ageb_path": In(input_manager_key="path_gpkg_manager"),
+    },
+    out=Out(io_manager_key="gpkg_manager"),
+)
+def translate_geometries_1990(
+    context: OpExecutionContext, ageb_path: Path, options_1990: str, options_2000: str
+) -> gpd.GeoDataFrame:
+    zone = context.partition_key
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_dir = Path(temp_dir)
+        temp_path = temp_dir / f"{zone}_temp.gpkg"
+        out_path = temp_dir / f"{zone}.gpkg"
+
+        gdal.VectorTranslate(str(temp_path), str(ageb_path), options=options_1990)
+        gdal.VectorTranslate(str(out_path), str(temp_path), options=options_2000)
+
+        os.remove(temp_path)
+
+        gdf = gpd.read_file(out_path)
+    return gdf
 
 
-@asset(
+def load_final_gcp_factory(year: int) -> OpDefinition:
+    @op(name=f"load_final_gcp_{year}")
+    def _op(context: OpExecutionContext, path_resource: PathResource) -> np.ndarray:
+        gcp_path = (
+            Path(path_resource.manual_path)
+            / f"gcp/{year}/{context.partition_key}.points"
+        )
+        points = pd.read_csv(
+            gcp_path, usecols=["sourceX", "sourceY", "mapX", "mapY"], header=1
+        )
+        points = points[
+            ["sourceX", "sourceY", "mapX", "mapY"]
+        ].to_numpy()  # Ensure right order
+        return points
+
+    return _op
+
+
+load_final_gcp_1990 = load_final_gcp_factory(1990)
+load_final_gcp_2000 = load_final_gcp_factory(2000)
+
+
+@op(
+    ins={
+        "ageb_path": In(input_manager_key="path_gpkg_manager"),
+    },
+    out=Out(io_manager_key="gpkg_manager"),
+)
+def translate_geometries_2000(
+    context: OpExecutionContext, ageb_path: Path, options: str
+) -> gpd.GeoDataFrame:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        out_path = Path(temp_dir) / f"{context.partition_key}.gpkg"
+        gdal.VectorTranslate(str(out_path), str(ageb_path), options=options)
+        gdf = gpd.read_file(out_path)
+    return gdf
+
+
+# pylint: disable=no-value-for-parameter
+@graph_asset(
     name="2000",
     key_prefix=["translated"],
-    ins={
-        "gcp_2000": AssetIn(key=["gcp", "final", "2000"]),
-        "ageb_path": AssetIn(
-            key=["zone_agebs", "shaped", "2000"], input_manager_key="path_gpkg_manager"
-        ),
-    },
+    ins={"ageb_path": AssetIn(["zone_agebs", "shaped", "2000"])},
     partitions_def=zone_partitions,
 )
-def translated_2000(
-    context: AssetExecutionContext,
-    path_resource: PathResource,
-    gcp_2000: np.ndarray,
-    ageb_path: Path,
-) -> None:
+def translated_2000(ageb_path: Path):
+    gcp_2000 = load_final_gcp_2000()
     options_str = generate_options_str(gcp_2000)
-    out_dir = prep_dir(path_resource, context)
-    out_path = out_dir / f"{context.partition_key}.gpkg"
-    gdal.VectorTranslate(str(out_path), str(ageb_path), options=options_str)
+    translated = translate_geometries_2000(ageb_path, options_str)
+    return translated
 
 
-@asset(
+# pylint: disable=no-value-for-parameter
+@graph_asset(
     name="1990",
     key_prefix=["translated"],
-    ins={
-        "gcp_1990": AssetIn(key=["gcp", "final", "1990"]),
-        "gcp_2000": AssetIn(key=["gcp", "final", "2000"]),
-        "ageb_path": AssetIn(
-            key=["zone_agebs", "shaped", "1990"], input_manager_key="path_gpkg_manager"
-        ),
-    },
+    ins={"ageb_path": AssetIn(key=["zone_agebs", "shaped", "1990"])},
     partitions_def=zone_partitions,
 )
 def translated_1990(
-    context: AssetExecutionContext,
-    path_resource: PathResource,
-    gcp_1990: np.ndarray,
-    gcp_2000: np.ndarray,
     ageb_path: Path,
 ) -> None:
-    options_str_1990 = generate_options_str(gcp_1990)
-    options_str_2000 = generate_options_str(gcp_2000)
+    gcp_1990 = load_final_gcp_1990()
+    gcp_2000 = load_final_gcp_2000()
 
-    out_dir = prep_dir(path_resource, context)
-    temp_path = out_dir / f"{context.partition_key}_temp.gpkg"
-    out_path = out_dir / f"{context.partition_key}.gpkg"
+    options_1990 = generate_options_str(gcp_1990)
+    options_2000 = generate_options_str(gcp_2000)
 
-    gdal.VectorTranslate(str(temp_path), str(ageb_path), options=options_str_1990)
-    gdal.VectorTranslate(str(out_path), str(temp_path), options=options_str_2000)
-
-    os.remove(temp_path)
+    translated = translate_geometries_1990(ageb_path, options_1990, options_2000)
+    return translated
